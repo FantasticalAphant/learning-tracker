5095d27835110ad0b5538a0d759ec3384f647c4f
David Lin <dlin88@binghamton.edu>
2023-08-18 15:35:50 -0400

    Save searched videos to database

--- src/main/java/io/github/generallyspecific/learningtracker/videos/VideoService.java
+++ src/main/java/io/github/generallyspecific/learningtracker/videos/VideoService.java
@@ -3,6 +3,9 @@ package io.github.generallyspecific.learningtracker.videos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.List;
+
 @Service
 public class VideoService {
     private final VideoRepository videoRepository;
@@ -15,6 +18,20 @@ public class VideoService {
     }
 
     public VideoResponse getVideo(String id) {
-        return videoClient.getVideo(id);
+        VideoResponse videoResponse = videoClient.getVideo(id);
+        List<Item> items = videoResponse.getItems();
+        Item item = items.get(0);
+
+        String videoId = item.getId();
+        String videoTitle = item.getSnippet().getTitle();
+        String videoDescription = item.getSnippet().getDescription();
+        String chanelId = item.getSnippet().getChannelId();
+        String channelTitle = item.getSnippet().getChannelTitle();
+        Instant publishedAt = Instant.parse(item.getSnippet().getPublishedAt());
+
+        Video video = new Video(videoId, videoTitle, videoDescription, chanelId, channelTitle, publishedAt);
+        videoRepository.save(video);
+
+        return videoResponse;
     }
 }
