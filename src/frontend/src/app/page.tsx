f44735f4d5dafea8fb00beaf7e1770dc01bc01d1
David Lin <dlin88@binghamton.edu>
2024-10-13 00:37:50 -0400

    Fix styling issue

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -1,6 +1,9 @@
+import {NavigationBar} from "@/components/NavigationBar";
+
 export default function Home() {
     return (
         <div>
+            <NavigationBar/>
             <h1>Test</h1>
         </div>
     );
