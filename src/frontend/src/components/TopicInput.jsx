83333f59108998e77172ab6ed92bd022fdc5b3c6
David Lin <dlin88@binghamton.edu>
2024-10-19 16:45:58 -0400

    Change UI components from Bootstrap to Tailwind

--- src/frontend/src/components/TopicInput.jsx
+++ src/frontend/src/components/TopicInput.jsx
@@ -1,5 +1,4 @@
 import {useState} from "react";
-import {Form, Button} from "react-bootstrap";
 
 export const TopicInput = ({onTopicsAdded}) => {
     const [name, setName] = useState("");
@@ -33,29 +32,46 @@ export const TopicInput = ({onTopicsAdded}) => {
 
     return (
         <>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formTitle">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter title"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group controlId="formBody">
-                    <Form.Label>Body</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        placeholder="Enter body"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </Form.Group>
-                <Button variant="primary" type="submit">
+            <form onSubmit={handleSubmit}>
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                    <label htmlFor="topic" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                        Topic Name
+                    </label>
+                    <div className="mt-2 sm:col-span-2 sm:mt-0">
+                        <input
+                            id="topic"
+                            name="topic"
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                    <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+                        <label htmlFor="description"
+                               className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+                            Description
+                        </label>
+                        <div className="mt-2 sm:col-span-2 sm:mt-0">
+                            <textarea
+                                id="description"
+                                name="description"
+                                rows={3}
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                className="block w-full max-w-2xl rounded-md border-0 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                defaultValue={''}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
                     Submit
-                </Button>
-            </Form>
+                </button>
+            </form>
         </>
     )
 }
