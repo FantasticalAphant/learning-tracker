907f6da18e0189be25dde0bd6c538731002c5335
David Lin <dlin88@binghamton.edu>
2024-10-21 16:02:01 -0400

    Display appropriate info in corresponding tabs for topic

--- src/frontend/src/app/topics/[id]/page.tsx
+++ src/frontend/src/app/topics/[id]/page.tsx
@@ -22,7 +22,7 @@ export default function IndividualTopicPage({params}: Params) {
     const {id} = params;
     const [topic, setTopic] = useState([]);
     const [articles, setArticles] = useState([]);
-    const [books, setBooks] = useState([]);
+    // const [books, setBooks] = useState([]);
     const [videos, setVideos] = useState([]);
     const [activeTab, setActiveTab] = useState(tabs[0]);
     const [isLoading, setIsLoading] = useState(false);
@@ -34,6 +34,7 @@ export default function IndividualTopicPage({params}: Params) {
                 const response = await fetch(`http://localhost:8080/topics/${id}`);
                 const topic = await response.json();
                 setTopic(topic);
+                console.log(topic);
             } catch (error) {
                 console.log(error);
             }
@@ -45,34 +46,37 @@ export default function IndividualTopicPage({params}: Params) {
         const fetchTopic = async () => {
             setIsLoading(true);
             try {
-                let endpoints = [];
-                if (activeTab.name === "All") {
-                    endpoints = ["/articles", "/books", "/videos"];
-                } else {
-                    endpoints = [activeTab.endpoint];
-                }
+                // let endpoints = [];
+                // if (activeTab.name === "All") {
+                //     endpoints = ["/articles", "/books", "/videos"];
+                // } else {
+                //     endpoints = [activeTab.endpoint];
+                // }
 
-                const results = await Promise.all(
-                    endpoints.map(endpoint => fetch(`http://localhost:8080${endpoint}/topic/${id}`).then(response => response.json()))
-                );
+                // const results = await Promise.all(
+                //     endpoints.map(endpoint => fetch(`http://localhost:8080${endpoint}/topic/${id}`).then(response => response.json()))
+                // );
 
-                const [topicResponse, articlesResponse] = await Promise.all([
-                    fetch(`http://localhost:8080/topics/${id}`),
-                    fetch(`http://localhost:8080/articles/topic/${id}`)
+                const [articlesResponse, videosResponse] = await Promise.all([
+                    fetch(`http://localhost:8080/articles/topic/${id}`),
+                    fetch(`http://localhost:8080/videos/topic/${id}`),
                 ]);
 
-                const topic = await topicResponse.json();
                 const articles = await articlesResponse.json();
+                const videos = await videosResponse.json();
 
-                setTopic(topic);
+                console.log(articles);
+                console.log(videos);
+
                 setArticles(articles);
+                setVideos(videos);
             } catch (error) {
                 console.log(error);
             }
         };
 
         fetchTopic().then(r => console.log(r));
-    }, [id])
+    }, [id, activeTab])
 
     return (
         <Shell highlightedTab={"Topics"}>
@@ -90,9 +94,15 @@ export default function IndividualTopicPage({params}: Params) {
             <h1>Books</h1>
 
             <h1>Videos</h1>
+            {videos && videos.map(video => (
+                <div key={video["id"]}>
+                    <h2>{video["title"]}</h2>
+                    <p>{video["description"]}</p>
+                </div>
+            ))}
 
-            <div className="border-b border-gray-200 pb-5 sm:pb-0">
-                <h3 className="text-base font-semibold leading-6 text-gray-900">Candidates</h3>
+            <div className="pt-5 border-b border-gray-200 pb-5 sm:pb-0">
+                <h3 className="text-base font-semibold leading-6 text-gray-900">{topic["name"]}</h3>
                 <div className="mt-3 sm:mt-4">
                     <div className="sm:hidden">
                         <label htmlFor="current-tab" className="sr-only">
@@ -101,7 +111,7 @@ export default function IndividualTopicPage({params}: Params) {
                         <select
                             id="current-tab"
                             name="current-tab"
-                            defaultValue={tabs.find((tab) => tab.current).name}
+                            defaultValue={tabs.find((tab) => tab.current)?.name}
                             className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                         >
                             {tabs.map((tab) => (
@@ -114,11 +124,11 @@ export default function IndividualTopicPage({params}: Params) {
                             {tabs.map((tab) => (
                                 <a
                                     key={tab.name}
-                                    href={"#"}
+                                    href={"javascript:;"}
                                     onClick={() => setActiveTab(tab)}
                                     aria-current={tab.current ? 'page' : undefined}
                                     className={classNames(
-                                        tab.current
+                                        tab.name === activeTab.name
                                             ? 'border-indigo-500 text-indigo-600'
                                             : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
                                         'whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium',
@@ -131,6 +141,21 @@ export default function IndividualTopicPage({params}: Params) {
                     </div>
                 </div>
             </div>
+
+            {(activeTab.name === "Articles" || activeTab.name === "All") && articles && articles.map(article => (
+                <div key={article["id"]}>
+                    <h2>{article["title"]}</h2>
+                    <p>{article["content"]}</p>
+                </div>
+            ))}
+
+            {(activeTab.name === "Videos" || activeTab.name === "All") && videos && videos.map(video => (
+                <div key={video["id"]}>
+                    <h2>{video["title"]}</h2>
+                    <p>{video["description"]}</p>
+                </div>
+            ))}
+
         </Shell>
     )
 }
