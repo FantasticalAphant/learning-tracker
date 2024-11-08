83b72042f03a2d1041dc5c6dc3927e66cf81d75f
David Lin <dlin88@binghamton.edu>
2024-11-07 22:19:01 -0500

    Prevent user from submitting topic without name

--- src/frontend/src/components/TopicInput.jsx
+++ src/frontend/src/components/TopicInput.jsx
@@ -65,7 +65,8 @@ export const TopicInput = ({onTopicsAdded}) => {
                 </div>
                 <button
                     type="submit"
-                    className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    disabled={!name.trim()}
+                    className={`rounded-md px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${name.trim() ? "bg-indigo-600 hover:bg-indigo-500" : "bg-indigo-300 cursor-not-allowed"}`}
                 >
                     Submit
                 </button>
