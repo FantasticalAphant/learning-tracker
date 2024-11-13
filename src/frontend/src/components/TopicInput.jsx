05daf758a489d8da6b0f60dfe77fdf2602aefc6e
David Lin <dlin88@binghamton.edu>
2024-11-13 11:51:05 -0500

    Disable resizing of textarea

--- src/frontend/src/components/TopicInput.jsx
+++ src/frontend/src/components/TopicInput.jsx
@@ -58,7 +58,7 @@ export const TopicInput = ({onTopicsAdded}) => {
                             rows={3}
                             value={description}
                             onChange={(e) => setDescription(e.target.value)}
-                            className="block w-full max-w-2xl rounded-md border-0 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            className="block w-full max-w-2xl rounded-md border-0 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 resize-none"
                             defaultValue={''}
                         />
                     </div>
