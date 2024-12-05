529e390d1b2f4b750bace91393b39bc662aea8fc
David Lin <davidlin9812@gmail.com>
2024-12-04 23:46:34 -0500

    Remove unnecessary default value

--- src/frontend/src/components/TopicInput.jsx
+++ src/frontend/src/components/TopicInput.jsx
@@ -62,7 +62,6 @@ export const TopicInput = ({onTopicsAdded}) => {
                             value={description}
                             onChange={(e) => setDescription(e.target.value)}
                             className="block w-full max-w-2xl rounded-md border-0 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 resize-none"
-                            defaultValue={''}
                         />
                     </div>
                 </div>
