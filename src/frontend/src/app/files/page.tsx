499b3d98008df6863dc5cb760e2b634da78e05f9
David Lin <davidlin9812@gmail.com>
2024-12-20 17:58:16 -0500

    Make some minor adjustments to files page

--- src/frontend/src/app/files/page.tsx
+++ src/frontend/src/app/files/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import Shell from "@/components/Shell";
-import {PhotoIcon} from "@heroicons/react/16/solid";
 import React, {useEffect, useState} from "react";
 import {S3File} from "@/types";
+import {DocumentPlusIcon} from "@heroicons/react/16/solid";
 
 function formatBytes(bytes: number): string {
     if (bytes === 0) return '0 Bytes';
@@ -77,11 +77,11 @@ export default function FilesPage() {
     return (
         <div>
             <Shell highlightedTab={"Files"}>
-                <p className="text-4xl text-center">Files</p>
+                <p className="text-4xl text-center mb-10">Files</p>
 
                 <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                     <div className="text-center">
-                        <PhotoIcon aria-hidden="true" className="mx-auto size-12 text-gray-300"/>
+                        <DocumentPlusIcon aria-hidden="true" className="mx-auto size-12 text-gray-300"/>
                         <div className="mt-4 flex text-sm/6 text-gray-600">
                             <label
                                 htmlFor="file-upload"
@@ -100,7 +100,7 @@ export default function FilesPage() {
                             </label>
                             <p className="pl-1">or drag and drop</p>
                         </div>
-                        <p className="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                        <p className="text-xs/5 text-gray-600">PNG, JPG, PDF up to 10MB</p>
                     </div>
                 </div>
 
