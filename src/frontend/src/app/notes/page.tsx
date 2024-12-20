79b76e6247f814aaad385e76c813a22a129735b8
David Lin <davidlin9812@gmail.com>
2024-12-20 15:09:28 -0500

    Allow notes page to render markdown

--- src/frontend/src/app/notes/page.tsx
+++ src/frontend/src/app/notes/page.tsx
@@ -1,6 +1,15 @@
 "use client"
 
 import Shell from "@/components/Shell";
+import ReactMarkdown from 'react-markdown'
+
+const markdownText = `
+# Heading 1
+**Bold** and *Italic* text
+
+- List item 1
+- List item 2
+`
 
 export default function NotesPage() {
     return (
@@ -8,6 +17,7 @@ export default function NotesPage() {
             <Shell highlightedTab={"Notes"}>
                 <p className="text-4xl text-center">Notes</p>
                 {/*TODO: Allow user to add notes and then link them to topics*/}
+                <ReactMarkdown className="prose">{markdownText}</ReactMarkdown>
             </Shell>
         </div>
     )
